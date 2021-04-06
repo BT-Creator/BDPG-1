ebb98c82806e94d863ccdf3acee0bdb6d294f24a
Bo <bo.robbrecht@student.howest.be>
2021-04-06 16:27:02 +0200

    Extracted discover functionality to functions.py

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 import pandas as pd
 from data.allowed_values import allowed_values
+from functions import discover_inconsistencies
 
 train = pd.read_csv('./data/train.csv')
-
-for column in train:
-    if (column != 'Id') and column in allowed_values:
-        illegal_values = train[~train[column].isin(allowed_values[column])][column]
-        illegal_rows = illegal_values.count()
-        if illegal_rows > 0:
-            print("There are", illegal_rows, "rows with illegal string values in column", column)
-            print("-" * 80)
-    elif column != 'Id':
-        train[column] = train[column].fillna(-1).astype(int)
-        illegal_rows = train[train[column] == -1][column].count()
-        if illegal_rows > 0:
-            print("There are", illegal_rows, "rows with NaN values in column", column)
-            print("-"*80)
+discover_inconsistencies(train, allowed_values)

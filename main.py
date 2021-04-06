b9f34be2323b94b974c44e49540d44566004255f
Bo <bo.robbrecht@student.howest.be>
2021-04-06 11:54:13 +0200

    Identified false NA values

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
 train = pd.read_csv('./data/train.csv')
-train.info()
+
+for column in train:
+    nan = train[column].isnull().values.any()
+    if (nan == True):
+        print(column)
+        print(nan)
+        print("-"*80)
+
+#for column in train:
+#    train.sort_values(column)
+#    print(column)
+#    print('-'*80)
+#    print(train[column].unique())
 
--- research_log.md
+++ research_log.md
@@ -1,4 +1,12 @@
 # 6-04-21
-## 9 - 11
+## Group
 - Discussed Project content
 - Search for additional dataset on https://data.boston.gov/ 
+
+## Bo
+- Found out that in `test.csv` the following values are wrongly labeled as NA:
+  - Lot Frontage
+  - GarageYrBlt
+  - MSZoing
+  - MasVnrArea
+  - Electrical

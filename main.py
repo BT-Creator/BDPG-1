dcb42aee308e0e88105475710cb4761dbf509291
Bo <bo.robbrecht@student.howest.be>
2021-04-06 16:23:22 +0200

    Able to find nan values in train dataset

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from data.allowed_values import allowed_values
 
 train = pd.read_csv('./data/train.csv')
@@ -9,4 +8,11 @@ for column in train:
         illegal_values = train[~train[column].isin(allowed_values[column])][column]
         illegal_rows = illegal_values.count()
         if illegal_rows > 0:
-            print("There are", illegal_rows, "rows with illegal values in column", column)
+            print("There are", illegal_rows, "rows with illegal string values in column", column)
+            print("-" * 80)
+    elif column != 'Id':
+        train[column] = train[column].fillna(-1).astype(int)
+        illegal_rows = train[train[column] == -1][column].count()
+        if illegal_rows > 0:
+            print("There are", illegal_rows, "rows with NaN values in column", column)
+            print("-"*80)

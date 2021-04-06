917d9c79b49637777d6d514d030ea5983f9acf93
Bo <bo.robbrecht@student.howest.be>
2021-04-06 17:56:24 +0200

    Replaced Illegal Characters in Bldgtype

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ discover_inconsistencies(train, allowed_values)
 # Cleaning Phase
 train.loc[train['LotFrontage'] == -1, 'LotFrontage'] = apply_avg(train, 'LotFrontage', -1)
 train.loc[train['MSZoning'] == "C (all)", 'MSZoning'] = replace_string(train, "MSZoning", "C (all)", "C")
+train.loc[train['BldgType'] == "Twnhs", 'BldgType'] = replace_string(train, "BldgType", "Twnhs", "TwnhsI")
+train.loc[train['BldgType'] == "2fmCon", 'BldgType'] = replace_string(train, "BldgType", "2fmCon", "2FmCon")
 
 # Verifying
 discover_inconsistencies(train, allowed_values)

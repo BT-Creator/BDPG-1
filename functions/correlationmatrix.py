84967edcbc4fcefc5147534403eefc083e2b62f4
lennert.commeine <lennert.commeine@student.howest.be>
2021-04-27 10:49:35 +0200

    added the correlationmatrix.py function

---
+++ functions/correlationmatrix.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import seaborn as sn
+
+
+def generate_correlation_matrix(df):
+    """function that generates the correlation matrix
+
+    :param df: Dataframe
+    :return: correlation matrix of DataFrame
+    """
+    corr_matrix = df.corr()
+    sn.heatmap(corr_matrix, annot=True)
+    return plt

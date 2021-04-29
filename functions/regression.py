be7464b4c135b85dcbd412dc7c940f622f32fc87
Bo <bo.robbrecht@student.howest.be>
2021-04-29 14:07:28 +0200

    Able to predict prices with limited set of indicators

--- functions/regression.py
+++ functions/regression.py
@@ -1,29 +1,30 @@
-import pandas as pd
 from sklearn.linear_model import LinearRegression
-
-from config.clean_params import categorical_values
+from config.clean_params import categorical_values, date_columns
 from config.regression_params import reg_params
 
 
 def split_data(train, test):
     train_data = train
-    test_data = test[reg_params.get('target_column')].values
-    ref_prices = test.drop(reg_params.get('target_column'), axis=1).values
+    test_data = test.drop(reg_params.get('target_column'), axis=1)
+    ref_prices = test[reg_params.get('target_column')]
     return train_data, test_data, ref_prices
 
 
 def linear_regression(train_data, test_data, ref_prices):
     l_reg = LinearRegression()
     train_data = prep_regression_data(train_data)
-    #l_reg.fit(train_data.values, ref_prices)
-    # print(l_reg.predict(train_data))
+    test_data = prep_regression_data(test_data)
+    l_reg.fit(test_data[["MSZoning", "LotFrontage"]].values, ref_prices.values)
+    print(l_reg.predict(test_data[["MSZoning", "LotFrontage"]].values))
 
 
 def prep_regression_data(df):
-    for key in categorical_values.keys():
+    categorical_columns = list(categorical_values.keys())
+    for key in categorical_columns:
         if df[key].isnull().values.any():
             df[key] = df[key].cat.add_categories(-1).fillna(-1)
             df[key] = df[key].cat.remove_categories(None)
         df[key] = df[key].cat.reorder_categories(df[key].unique(), ordered=True)
         df[key] = df[key].cat.codes
+    df[df.columns.difference(categorical_columns)].fillna(-1)
     return df

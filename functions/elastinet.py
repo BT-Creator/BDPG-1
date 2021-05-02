b5a4f66fda626363a52a771d712dd85d777201bc
Bo <bo.robbrecht@student.howest.be>
2021-05-02 15:08:02 +0200

    Able to predict prices of target with ElasticNet regression

--- functions/elastinet.py
+++ functions/elastinet.py
@@ -5,27 +5,22 @@ from config.regression_params import reg_params
 from functions.regression_helper import prep_regression_data, split_data, print_results
 
 
-def elastinet_regression(train):
-    prep = prep_regression_data(train)
-    X, y = split_data(prep)
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X,
-        y,
-        test_size=reg_params.get('test_size'),
-        random_state=reg_params.get('random_state')
-    )
+def elastinet_regression(ref, target):
+    prepped_ref = prep_regression_data(ref)
+    prepped_target = prep_regression_data(target)
+    prepped_ref.drop(ref.tail(1).index, inplace=True)
+    ref_data, ref_prices = split_data(prepped_ref)
 
     # Create the regressor: reg_all
     elastic = ElasticNet(random_state=reg_params.get('random_state'))
 
     # Fit the regressor to the training data
-    elastic.fit(X_train, y_train)
+    elastic.fit(ref_data, ref_prices)
 
     # Predict on the test data: y_pred
-    y_pred = elastic.predict(X_test)
+    y_pred = elastic.predict(prepped_target.values)
 
     # Compute and print R^2 and RMSE
-    return print_results(elastic, X_test, y_test, y_pred, "ElasticNet")
+    return print_results(elastic, ref_data, ref_prices, y_pred, "ElasticNet")
 
 
